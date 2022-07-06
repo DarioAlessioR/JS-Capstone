22239cfe6ae802fa234727ca109a44f05fd27f59
Maury PC <tryptic-st@outlook.com>
2022-07-06 16:44:33 -0500

    Add linters and check for errors

--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
-
 import './style.css';
-
 
 const urlFood = 'https://www.themealdb.com/api/json/v1/1/categories.php';
 const urlData = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/';
-const urlDataId = 'Lg1NwTSFJSG37nTmEN8x'
-const urlDataLikes = `https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/${urlDataId}/likes/`
-const urlDataComments = `https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/${urlDataId}/comments/`
+const urlDataId = 'Lg1NwTSFJSG37nTmEN8x';
+const urlDataLikes = `https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/${urlDataId}/likes/`;
+const urlDataComments = `https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/${urlDataId}/comments/`;
 
 const url2 = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/4UvdTn5NAXeyK3iSlBaQ/likes/';
-
 
 const getData = async () => {
   const results = await fetch(urlFood);
@@ -29,7 +26,7 @@ const getData = async () => {
 const printfood = (result, likesObj) => {
   const prueba1 = document.getElementById('prueba1');
   prueba1.innerHTML = '';
-  result.forEach(element => {
+  result.forEach((element) => {
     const v = element.idCategory;
     const x = element.strCategory;
     const z = element.strCategoryThumb;
@@ -37,57 +34,57 @@ const printfood = (result, likesObj) => {
       let w = '';
       if (likesObj[i].item_id === v) {
         w = likesObj[i].likes;
-         
-        const food = document.createElement('li')
-        food.innerHTML = `
-        <img class="platillo-img" src=${z} alt="">
-        <div class="platillo-content">
-            <p class="platillo-title">${x}</p>
 
-            <p class="platillo-likes"><span class="likes">${w}</span>
-            
-            <span class="heart" id=${v}>&#10084</span> likes</p>
-        </div>
-        <div class="platillo-buttons">
-            <button class="platillo-comments" id="comments" type="button">Comments</button>
-            <button class="platillo-reservations" id="reservations" type="button">Reservations</button>
-        </div>
+        const food = document.createElement('li');
+        food.innerHTML = `
+    <div class="image-container">
+    <img class="platillo-img" src=${z} alt="">
+    </div>
+    <div class="platillo-content">
+    <p class="platillo-title">${x}</p>
+
+    <p class="platillo-likes"><span class="likes">${w}</span>
+
+        <span class="heart" id=${v}>&#10084</span> likes
+    </p>
+    </div>
+    <div class="platillo-buttons">
+    <button class="platillo-comments" id="comments" type="button">Comments</button>
+    <button class="platillo-reservations" id="reservations" type="button">Reservations</button>
+    </div>
         `;
         prueba1.appendChild(food);
       }
     }
   });
-  const hearts = document.querySelectorAll('.heart')
+  const hearts = document.querySelectorAll('.heart');
   captureLike(hearts);
 };
 
 const captureLike = (hearts) => {
-   
-  hearts.forEach(element => {
+  hearts.forEach((element) => {
     element.addEventListener('click', () => {
-        
       postLike(element.id);
     });
-  })
+  });
 };
 
 const postLike = async (id) => {
   const response = await fetch(urlDataLikes, {
     method: 'POST',
     body: JSON.stringify({
-      item_id: id
+      item_id: id,
     }),
     headers: {
       'Content-Type': 'application/json;charset=utf-8',
     },
-  })
+  });
   const data = await response.text();
   getData();
   return data;
-}
+};
 
 getData();
-
 
 /*
 const newApp = async () => {
--- src/tests/prueba.test.js
+++ src/tests/prueba.test.js
@@ -2,11 +2,10 @@
 /**
  * @jest-environment jsdom
  */
- import sum from '../modules/prueba.js';
- 
- 
- describe('adds 1 + 2 to equal 3', () => {
-   test('add 2 numbers', () => {
+import sum from '../modules/prueba.js';
+
+describe('adds 1 + 2 to equal 3', () => {
+  test('add 2 numbers', () => {
     expect(sum(1, 2)).toBe(3);
-   });
- });
+  });
+});
